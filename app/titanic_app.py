6e7cdea3c53e6eb87d42dde0025ce4b1e3e174fe
Manuel Götz <manuel@goetz.eu>
2026-01-31 18:21:52 +0100

    Make title selectable

--- app/titanic_app.py
+++ app/titanic_app.py
@@ -16,6 +16,32 @@ import streamlit as st
 st.title("🚢 Titanic Survival Predictor")
 st.write("Enter passenger details to predict survival probability.")
 
+TITLE_RULES = {
+    "male": {
+        "child": ["Master"],
+        "adult": [
+            "Mr", "Dr", "Rev",
+            "Capt", "Col", "Major",
+            "Sir", "Don", "Jonkheer"
+        ]
+    },
+    "female": {
+        "child": ["Miss"],
+        "adult": [
+            "Miss", "Mlle", "Ms",
+            "Mrs", "Mme", "Dr",
+            "Lady", "the Countess", "Dona"
+        ]
+    }
+}
+
+def get_available_titles(sex, age):
+    if age < 14:
+        return TITLE_RULES[sex]["child"]
+    else:
+        return TITLE_RULES[sex]["adult"]
+
+
 # --- User inputs ---
 sex = st.selectbox("Sex", ["male", "female"])
 pclass = st.selectbox("Passenger Class", [1, 2, 3])
@@ -24,7 +50,12 @@ fare = st.number_input("Fare", min_value=0.0, value=30.0)
 sibsp = st.number_input("Number of Siblings + Spouses on Board", 0, 8, 0)
 parch = st.number_input("Number of Parents + Children on Board", 0, 6, 0)
 embarked = st.selectbox("Embarked", ["Southampton (ENG)", "Cherbourg (FRA)", "Queenstown (IRL)"])
-name = st.text_input("Names", "Doe, Mr. John")
+name = st.text_input("Name", "Doe, Mr. John")
+available_titles = get_available_titles(sex, age)
+title = st.selectbox(
+    "Title",
+    available_titles
+)
 cabin = st.text_input("Cabin (optional)", "")
 ticket = st.text_input("Ticket", "A/5 21171")
 
@@ -37,7 +68,7 @@ ticket = st.text_input("Ticket", "A/5 21171")
 #     "SibSp": sibsp,
 #     "Parch": parch,
 #     "Embarked": embarked, # only first letter
-#     "Name": name,
+#     "Name": title,
 #     "Cabin": cabin if cabin else None,
 #     "Ticket": ticket
 # }])
